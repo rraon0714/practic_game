4311016f28de619c44570d963be171a01acedeb2
windmi95 <rrao0714@gmail.com>
2025-04-09 00:46:39 +0900

    practice_7

--- Assets/Script/BoxGenerator.cs
+++ Assets/Script/BoxGenerator.cs
@@ -7,24 +7,33 @@ public class BoxGeneratorScript : MonoBehaviour
     float span = 1.0f;
     float beforeY = 0.0f;
     float boforeX = 0.0f;
-    float speed = 2.0f;
+    float speed = 0.5f;
     private Rigidbody2D rb;
-    float direction = 3.0f;
+    private float direction = 3.0f;
+    public float range = 2.0f;
+
+    private float starX;
 
     float currentPosition;
     float rightMax = 2.0f;
     float leftMax = -2.0f;
-    float distanceMax = 1f;
+    float distanceMax = 1.0f;
     public float refNum = 0f;
 
-    //private float _xMovieSpeed;
-    //private float _xDelta = 2;
-    //private float _xStarPosition;
+    private bool isOnMovingPlatform = false;
+    
+    private Vector3 currentPlatform;
+    private bool isGrounded = false;
+
+    //public float DestructionDelay = 4.0f;
+    private Vector2 limitMin = new Vector2(-13.0f, -4.0f);
+    private Vector2 limitMax = new Vector2(13.0f, 4.0f);
 
 
 
     void Start()
     {
+        starX = transform.position.x;
         rb = GetComponent<Rigidbody2D>();
         beforeY = -3;
         currentPosition = transform.position.x;
@@ -32,49 +41,41 @@ public class BoxGeneratorScript : MonoBehaviour
 
     void Update()
     {
+        transform.Translate(Vector3.right * speed * direction * Time.deltaTime);
 
+        if(Mathf.Abs(transform.position.x - starX) > range)
+        {
+            direction *= -1;
+        }
+
+        // 객체 랜덤으로 생성
         delta += Time.deltaTime;
         if (delta > span)
         {
             delta = 0;
-            GameObject newBox = Instantiate(BoxPrefab);
-            //GameObject gameObject = Instantiate(BoxPrefab) as GameObject;
-
-            //랜덤으로 x,y 객체 생성
             float positionX = Random.Range(boforeX - 4.0f, boforeX + 4.0f);
             float positionY = Random.Range(1.8f, 2.3f);
             beforeY += positionY;
             boforeX = positionX;
+
+            // 객체 좌우로 움직이기
+            GameObject newBox = Instantiate(BoxPrefab);
             newBox.transform.position = new Vector3(positionX, beforeY, 0);
-
-
-            //gameObject.transform.position = new Vector3(positionX, beforeY, 0);
-            //gameObject.transform.Translate(new Vector3(3, 0, 0));
+            transform.Translate(distanceMax * Time.deltaTime * Mathf.Sin(refNum + Time.time * speed),0,0); 
 
             Rigidbody2D rb = newBox.GetComponent<Rigidbody2D>();
             if (rb != null)
             {
-                rb.velocity = new Vector2(speed * direction, 0);
+                rb.linearVelocity = new Vector2(speed * direction, 0);
             }
-
-
-            //currentPosition += Time.deltaTime * direction;
-            //if (currentPosition >= rightMax)
-            //{
-            //    direction *= -1;
-            //    currentPosition = rightMax;
-            //}
-            //else if (currentPosition <= leftMax)
-            //{
-            //    direction *= -1;
-            //    currentPosition = leftMax;
-            //}
-
-            //transform.position = new Vector3(currentPosition, 0, 0);
-            //transform.Translate(distanceMax * Time.deltaTime * Mathf.Sin(refNum + Time.time * speed), 0, 0);
-
-            //float x = _xStarPosition + _xDelta * Mathf.Sin(Time.time * _xMovieSpeed);
-            //transform.position = new Vector3(x, transform.position.y, 0);
+            
+            // 일정 시간 후에 객체 사라지기
+            //Destroy(newBox, DestructionDelay);
+            if(transform.position.x < limitMin.x || transform.position.x > limitMax.x || 
+               transform.position.y < limitMin.y || transform.position.y > limitMax.y)
+            {
+                Destroy(newBox);
+            }
         }
     }
-}
+}          
